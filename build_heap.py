b0f87334e85a6544a8bcdc97affa66b44a9c7fba
Evelīna Geikina <118215390+evelina1701@users.noreply.github.com>
2023-03-06 13:14:01 +0000

    123

--- build_heap.py
+++ build_heap.py
@@ -7,14 +7,14 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
     size=len(data)
-    left = 2*i+1
-    right = 2*i+2
+    left_node = 2*i+1
+    right_node = 2*i+2
 
     for i in range(n//2,-1,-1):
-        if left<size and data[left]<data[smallest]:
-            smallest = left
-        if right<size and data[right]<data[smallest]:
-            smallest = right
+        if left_node<size and data[left_node]<data[smallest]:
+            smallest = left_node
+        if right_node<size and data[right_node]<data[smallest]:
+            smallest = right_node
         if i != smallest:
             swaps.append((i,smallest))
             (data[i],data[smallest])=(data[smallest],data[i])
@@ -29,11 +29,20 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    letter = input()
+    if "F" in letter:
+        fileName = input()
+        if "a" in fileName:
+            return
+        with open(f"./test/{fileName}", mode="r") as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split())) 
+    if "I" in letter:
+        n = int(input())
+        data = list(map(int, input().split())) 
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
